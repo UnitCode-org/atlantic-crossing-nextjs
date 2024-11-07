ff621ee45f70464829de0dca4a3a644fc7b44684
JJ-unit <justin.jap@unitcode.org>
2024-11-07 09:52:39 +0700

    style: fixed navbar active style

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -27,7 +27,7 @@ function Navbar({ personId }: { personId: number | undefined }) {
         className={cn(
           'text-base font-semibold py-5 min-w-24',
           pathname.includes('/leaderboard')
-            ? 'bg-[#F3EFFD] text-primary'
+            ? 'bg-[#F3EFFD] text-primary hover:bg-[#F3EFFD] hover:text-primary'
             : 'bg-transparent text-[#8E8E93] hover:bg-[#F3EFFD] hover:text-primary/80'
         )}
       >
@@ -38,7 +38,7 @@ function Navbar({ personId }: { personId: number | undefined }) {
         className={cn(
           'text-base font-semibold py-5 min-w-24',
           pathname.includes('/matches')
-            ? 'bg-[#F3EFFD] text-primary'
+            ? 'bg-[#F3EFFD] text-primary hover:bg-[#F3EFFD] hover:text-primary'
             : 'bg-transparent text-[#8E8E93] hover:bg-[#F3EFFD] hover:text-primary/80'
         )}
       >
