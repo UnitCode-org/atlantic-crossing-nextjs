ef433064802815b59d057d956101d30da1f4b59e
maxx2002 <max.wiranata@unitcode.org>
2024-11-08 14:53:11 +0700

    feat: added hamburger menu

--- src/app/_components/WelcomeCard.tsx
+++ src/app/_components/WelcomeCard.tsx
@@ -1,14 +1,25 @@
-'use client';
+"use client";
 
-import { Button } from '@/components/ui/button';
-import Card from '@/components/ui/card';
-import { Command, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from '@/components/ui/command';
-import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
-import { cn } from '@/lib/utils';
-import { Person } from '@prisma/client';
-import { ArrowDown2, ArrowSwapHorizontal, TickCircle } from 'iconsax-react';
-import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { Button } from "@/components/ui/button";
+import Card from "@/components/ui/card";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { cn } from "@/lib/utils";
+import { Person } from "@prisma/client";
+import { ArrowDown2, ArrowSwapHorizontal, TickCircle } from "iconsax-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 function WelcomeCard({ persons }: { persons: Person[] }) {
   const router = useRouter();
@@ -17,25 +28,31 @@ function WelcomeCard({ persons }: { persons: Person[] }) {
   const [value, setValue] = useState(persons[0].id.toString());
 
   return (
-    <Card className='text-center w-[28rem]'>
-      <h1 className='text-primary text-base md:text-2xl font-semibold pt-4 pb-16'>Atlantic Crossing</h1>
-      <div className='flex flex-col gap-4'>
-        <h3 className='font-semibold text-[20px] md:text-[32px] pb-2'>What is your name?</h3>
+    <Card className="text-center w-[28rem]">
+      <h1 className="text-primary text-base md:text-2xl font-semibold pt-4 pb-16">
+        Atlantic Crossing
+      </h1>
+      <div className="flex flex-col gap-4">
+        <h3 className="font-semibold text-[20px] md:text-[32px] pb-2">
+          What is your name?
+        </h3>
         <Popover open={open} onOpenChange={setOpen}>
           <PopoverTrigger asChild>
             <Button
-              variant='outline'
-              role='combobox'
+              variant="outline"
+              role="combobox"
               aria-expanded={open}
-              className='justify-between border-none text-sm md:text-base font-normal bg-[#F6F7FE] py-7 px-5'
+              className="justify-between border-none text-sm md:text-base font-normal bg-[#F6F7FE] py-7 px-5"
             >
-              {value ? persons.find((person) => person.id === parseInt(value))?.name : 'Select name...'}
-              <ArrowDown2 color='#2C2C2E' size={24} />
+              {value
+                ? persons.find((person) => person.id === parseInt(value))?.name
+                : "Select name..."}
+              <ArrowDown2 color="#2C2C2E" size={24} />
             </Button>
           </PopoverTrigger>
-          <PopoverContent className='w-[320px] p-0' align='start'>
+          <PopoverContent className="w-[320px] p-0" align="start">
             <Command>
-              <CommandInput placeholder='Search name...' />
+              <CommandInput placeholder="Search name..." />
               <CommandList>
                 <CommandEmpty>No data found.</CommandEmpty>
                 <CommandGroup>
@@ -51,7 +68,12 @@ function WelcomeCard({ persons }: { persons: Person[] }) {
                     >
                       {person.name}
                       <TickCircle
-                        className={cn('ml-auto', value === person.id.toString() ? 'opacity-100' : 'opacity-0')}
+                        className={cn(
+                          "ml-auto",
+                          value === person.id.toString()
+                            ? "opacity-100"
+                            : "opacity-0"
+                        )}
                       />
                     </CommandItem>
                   ))}
@@ -61,13 +83,13 @@ function WelcomeCard({ persons }: { persons: Person[] }) {
           </PopoverContent>
         </Popover>
         <Button
-          className='py-6 text-sm md:text-base font-semibold rounded-lg my-2'
+          className="py-6 text-sm md:text-base font-semibold rounded-lg my-2"
           onClick={() => router.push(`/meet/${value}`)}
         >
           Get Started
         </Button>
-        <p className='flex items-center gap-3 text-sm md:text-base justify-center pb-16'>
-          <ArrowSwapHorizontal size={18} color='#6A45FF' />
+        <p className="flex items-center gap-3 text-sm md:text-base justify-center pb-16">
+          <ArrowSwapHorizontal size={18} color="#6A45FF" />
           288 Meets and Interactions
         </p>
       </div>
